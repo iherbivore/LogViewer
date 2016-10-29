07524864a2d54a00d9d0d6e1eecb9b344d63b37e
iherbivore <andrewcwinter@gmail.com>
2016-10-29 13:42:19 -0400

    Update LogCollection.php

--- src/Entities/LogCollection.php
+++ src/Entities/LogCollection.php
@@ -112,7 +112,7 @@ class LogCollection extends Collection
             $currentPage
         );
 
-        return $paginator->setPath($request->url());
+        return $paginator->setPath(route($request->url()));
     }
 
     /**
